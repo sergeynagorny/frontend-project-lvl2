dbc04289258a3f871d71e13909dda23c1edf3fd9
sergeynagorny <sergey.nagornyj@techvice.org>
2021-10-07 21:13:05 +0300

    fix: linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,17 @@
-#!/usr/bin/env node
-import { program } from "commander";
-import genDiff from "../src/gendiff.js";
+#!/usr/bin/env node --experimental-specifier-resolution=node
+import { program } from 'commander';
+import genDiff from '../src/gendiff';
 
 program
-    .argument('<first-file>', 'first file')
-    .argument('<second-file>', 'second file')
-    .action((ff, sf) => {
-        console.log(genDiff(ff, sf))
-    })
+  .argument('<first-file>', 'first file')
+  .argument('<second-file>', 'second file')
+  .action((ff, sf) => {
+    console.log(genDiff(ff, sf));
+  });
 
 program
-    .description('Compares two configuration files and shows a difference')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
-    .helpOption('-h, --help', 'output usage information')
-    .parse(process.argv);
+  .description('Compares two configuration files and shows a difference')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .parse(process.argv);
