e0adb4e1342a45da7e1d3728a040be83e73576a9
sergeynagorny <sergey.nagornyj@techvice.org>
2021-10-11 23:33:48 +0300

    refactor: parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,23 +1,7 @@
 import { readFileSync } from 'fs';
 import { extname, resolve } from 'path';
 import { load } from 'js-yaml';
-
-const ParserType = {
-  YAML: 'YAML',
-  JSON: 'JSON',
-};
-
-export const FileExt = {
-  JSON: '.json',
-  YAML: '.yaml',
-  YML: '.yml',
-};
-
-const ParserTypeByFileExt = {
-  [FileExt.YAML]: ParserType.YAML,
-  [FileExt.YML]: ParserType.YAML,
-  [FileExt.JSON]: ParserType.JSON,
-};
+import { ParserType, ParserTypeByFileExt } from './const';
 
 export function readFileData(filePath) {
   return readFileSync(resolve(process.cwd(), filePath));
@@ -38,7 +22,7 @@ export function parseDataByFileExt(file, fileExt) {
   }
 }
 
-export function parseDataByFile(filePath) {
+export default function parseDataByFile(filePath) {
   const file = readFileData(filePath);
   const ext = readFileExt(filePath);
   return parseDataByFileExt(file, ext);
