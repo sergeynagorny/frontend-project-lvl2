92b7e5cd29859e59819d98e145f32c553f3306e7
sergeynagorny <sergey.nagornyj@techvice.org>
2021-10-11 23:32:38 +0300

    feat: const

---
+++ src/const.js
@@ -0,0 +1,32 @@
+export const DiffType = {
+  ADDED: 'ADDED',
+  REMOVED: 'REMOVED',
+  UNCHANGED: 'UNCHANGED',
+  CHANGED: 'CHANGED',
+  NESTED: 'NESTED',
+};
+
+export const PrefixByDiff = {
+  [DiffType.ADDED]: '+',
+  [DiffType.REMOVED]: '-',
+  [DiffType.NESTED]: ' ',
+  [DiffType.CHANGED]: ' ',
+  [DiffType.UNCHANGED]: ' ',
+};
+
+export const ParserType = {
+  YAML: 'YAML',
+  JSON: 'JSON',
+};
+
+export const FileExt = {
+  JSON: '.json',
+  YAML: '.yaml',
+  YML: '.yml',
+};
+
+export const ParserTypeByFileExt = {
+  [FileExt.YAML]: ParserType.YAML,
+  [FileExt.YML]: ParserType.YAML,
+  [FileExt.JSON]: ParserType.JSON,
+};
